391b7e0d1f69f273bdbd5f8f83b8609ad2cbf859
Aleksan-dres <galeks382@gmail.com>
2025-03-31 21:06:39 +0300

    Update weather.py
    Поправочка

--- weather.py
+++ weather.py
@@ -1,10 +1,14 @@
 import requests
+
+
 def main():
-    places = ["Лондон","Шереметьево","Череповец"] 
-    payload = {"nTqmM&":"","lang":"ru"} 
-    for place in places: 
-        response = requests.get(f"https://wttr.in/{place}",params=payload )
-        response.raise_for_status() 
+    places = ["Лондон", "Шереметьево", "Череповец"]
+    payload = {"nTqmM": "", "lang": "ru"}
+    for place in places:
+        response = requests.get(f"https://wttr.in/{place}", params=payload)
+        response.raise_for_status()
         print(response.text)
-if __name__ =='__main__': 
+
+
+if __name__ == '__main__':
     main()
